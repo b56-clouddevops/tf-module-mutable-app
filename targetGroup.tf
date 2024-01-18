530f7272c476f481e9a1ec2719feb4dc1555e88b
CodingManoj <CodingManoj@gmail.com>
2024-01-17 20:49:32 -0500

    TF Backend Infra Auto Push

--- targetGroup.tf
+++ targetGroup.tf
@@ -4,6 +4,15 @@ resource "aws_lb_target_group" "app" {
   port        = var.APP_PORT
   protocol    = "HTTP"
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  health_check {
+    path                = "/health"
+    enabled             = true
+    interval            = 5
+    timeout             = 4
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 }
 
 # Attches the instances to the target group
