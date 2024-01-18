2f5dfdea8c66ac2aeda90e396d9b65fa60033791
CodingManoj <CodingManoj@gmail.com>
2024-01-17 20:18:37 -0500

    TF Backend Infra Auto Push

---
+++ app.tf
@@ -0,0 +1,18 @@
+resource "null_resource" "app_deploy" {
+  count   = local.INSTANCE_COUNT
+
+  provisioner "remote-exec" {
+
+    # connection block establishes connection to this
+    connection {
+      type     = "ssh"
+      user     = local.SSH_USERNAME
+      password = local.SSH_PASSWORD
+      host     = element(local.INSTANCE_IPS, count.index)             # aws_instance.sample.private_ip : Use this only if your provisioner is outside the resource.
+    }
+
+    inline = [
+      "ansible-pull -U https://github.com/b56-clouddevops/ansible.git  -e ENV=${var.ENV} -e COMPONENT=${var.COMPONENT}  roboshop-pull.yml"
+        ]
+    }
+}
