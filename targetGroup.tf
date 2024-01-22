f3f0d48ea41bfa776103df6db1318732a50918d5
CodingManoj <CodingManoj@gmail.com>
2024-01-21 19:46:03 -0500

    TF Backend Infra Auto Push

--- targetGroup.tf
+++ targetGroup.tf
@@ -6,11 +6,11 @@ resource "aws_lb_target_group" "app" {
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
 
   health_check {
-    path                = "/health"
-    enabled             = true
-    interval            = 5
+    enabled             = true 
+    healthy_threshold   = 2 
+    interval            = 5 
     timeout             = 4
-    healthy_threshold   = 2
+    path                = "/health"
     unhealthy_threshold = 2
   }
 }
